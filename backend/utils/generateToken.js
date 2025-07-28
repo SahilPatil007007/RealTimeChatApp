e7a995e0b47e70ad970cdc6b6970ed70f2b5c314
Sahil Patil <sahilpatil221004@gmail.com>
2025-07-28 23:24:27 +0530

    Cookie Configuration

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userId, res) => {
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "10m",
+		expiresIn: "48h",
 	});
 
 	res.cookie("jwt", token, {
-		maxAge: 10 * 60 * 1000, // MS
+		maxAge: 48 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-		secure: process.env.NODE_ENV !== "development",
+		sameSite: "none", // Allow cross-site requests
+		secure: true, // Required for sameSite: "none"
 	});
 };
 
